20cf65da6bb1e584befc17f38ddf98ea021d9478
joewildes01 <70964070+joewildes01@users.noreply.github.com>
2021-09-13 16:55:18 +0100

    Explore controller CSS additions

---
+++ PavillionMedical/Scripts/Custom.js
@@ -0,0 +1,39 @@
+﻿/* _layout js */
+var isOpen = true;
+
+function toggleSidebar() {
+    if (isOpen) {
+        document.getElementById("mySidenav").style.width = "300px";
+        this.isOpen = false;
+    }
+    else {
+        document.getElementById("mySidenav").style.width = "70px";
+        this.isOpen = true;
+    }
+}
+
+function openNav() {
+    document.getElementById("mySidenavTwo").style.width = "250px";
+    isOpen = true;
+}
+
+function closeNav() {
+    document.getElementById("mySidenavTwo").style.width = "0px";
+}
+
+/* Explore js */
+
+var isOpen = true;
+
+function toggleMoreInfo() {
+    if (isOpen) {
+        document.getElementById("more-info").style.height = "200px";
+        this.isOpen = false;
+    }
+    else {
+        document.getElementById("more-info").style.height = "0px";
+        this.isOpen = true;
+    }
+
+}
+var isOpen = true;
